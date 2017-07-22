717b95943cac45e85681e75911a9a472f86108c5
smitasengupta <smita_m@yahoo.com>
2017-07-22 12:29:13 -0400

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,5 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+yearlyTotal<-aggregate(NEI$Emissions,by=list(Year=NEI$year),sum)
+yearlyTotal$y <- round(yearlyTotal$x/1000,2)
+barplot(yearlyTotal$y, xlab="Year", ylab=expression(paste('PM', ''[2.5], ' in Kilotons')), main=expression(paste('Total Emission ')), names.arg = yearlyTotal$Year, border="white", col=rainbow(5),  density=c(200))
