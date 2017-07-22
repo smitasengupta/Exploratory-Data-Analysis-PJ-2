6ada6d092aa7f2d66e26d7b8686e88684ca75da4
smitasengupta <smita_m@yahoo.com>
2017-07-22 11:45:13 -0400

    Delete plot1.r

--- plot1.r
+++
@@ -1,5 +0,0 @@
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-yearlyTotal<-aggregate(NEI$Emissions,by=list(Year=NEI$year),sum)
-yearlyTotal$y <- round(yearlyTotal$x/1000,2)
-barplot(yearlyTotal$y, xlab="Year", ylab=expression(paste('PM',''[2.5],'in Kilotons')), main=expression(paste('Total Emission of PM'[2.5])),names.arg = yearlyTotal$Year)
